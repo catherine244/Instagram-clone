9a7526a5669bc78bd12e28ae4d84486f4aa59c66
catherine244 <ccayreen24@gmail.com>
2020-02-09 17:29:17 +0300

    added class postlistview in views.py

--- Instagram/views.py
+++ Instagram/views.py
@@ -4,3 +4,15 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    posts= Image.objects.all(),
+    commentform= CommentForm()
+    
+    return render(request, 'index.html', locals())
+
+class PostListView(LoginRequiredMixin,ListView):
+    model=Image
+    template_name= 'instagram/image_list.html' # <app>/<model>_<view_type>.html
+    
+    context_object_name = 'posts'
+    ordering = ['-time_created']
