149d8b6a46071788754e4f5e6a91b55f567fbf19
catherine244 <ccayreen24@gmail.com>
2020-02-09 18:07:34 +0300

    registered my models in admin.py

--- Instagram/admin.py
+++ Instagram/admin.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.contrib import admin
+from .models import Image,Comment,Like
 
 # Register your models here.
+admin.site.register(Image)
+admin.site.register(Comment)
+admin.site.register(Like)
