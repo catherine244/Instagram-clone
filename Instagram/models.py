d9249b834e9fe770e29e64e0a6cbf0be6afc31ea
catherine244 <ccayreen24@gmail.com>
2020-02-09 17:16:59 +0300

    added class Image in models

--- Instagram/models.py
+++ Instagram/models.py
@@ -3,4 +3,41 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+# Create your models here.class Image(models.Model):
+    image = models.ImageField(upload_to='images/')
+    author = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE,related_name="yg")
+    title = models.CharField(max_length=60, null=True)
+    time_created = models.DateTimeField(auto_now=True, auto_now_add=False)
+    likes = models.IntegerField(default=0, null=True)
+    caption = models.CharField(max_length=140, default="")
+    user_profile = models.ForeignKey(Profile, on_delete=models.CASCADE,null=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('home')
+    def save_image(self):
+        self.save()
+
+    @classmethod
+    def retrieve_all(cls):
+        all_objects = Image.objects.all()
+        for item in all_objects:
+            return item;
+
+    @classmethod
+    def get_image_by_id(cls,incoming_id):
+        image_result = cls.objects.get(id=incoming_id)
+        return image_result
+
+
+    @classmethod
+    def update_image(cls,current_value,new_value):
+        fetched_object = Image.objects.filter(author=current_value).update(author=new_value)
+        return fetched_object
+
+
+
+
