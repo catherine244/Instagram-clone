a64d726a4c71deef2caa8474d9ec06b9f33b820f
catherine244 <ccayreen24@gmail.com>
2020-02-09 17:56:12 +0300

    added class PhotoUploadModelForm in forms.py

--- Instagram/forms.py
+++ Instagram/forms.py
@@ -1,3 +1,10 @@
 rom django import forms
 from .models import Image,Comment
 from user.models import Profile
+
+
+class PhotoUploadModelForm(forms.ModelForm):
+    
+    class Meta:
+        model = Image
+        fields = [ 'title','image','caption']
