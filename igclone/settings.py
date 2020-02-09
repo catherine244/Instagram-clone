ec74f642b98a9e856e842233c20b39d2b5b5e65c
catherine244 <ccayreen24@gmail.com>
2020-02-09 18:54:27 +0300

    added database in settings.py

--- igclone/settings.py
+++ igclone/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'igclone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'igi',
+        'USER': 'cate',
+        'PASSWORD': 'cate1234',
     }
 }
 
