1009a52536853eae86cbe97fd1b256c7eba5d98b
catherine244 <ccayreen24@gmail.com>
2020-02-10 10:59:18 +0300

    added Description in my README.md

---
+++ README.md
@@ -0,0 +1,9 @@
+## PROJECT NAME 
+ - AvechiGram
+
+ ## Author 
+ - Catherine Nduku 
+
+ ## Description 
+
+ This is a clone of Instagram. You can post, comment and view pictures posted on this App just like on Instagram.
--- instagram/tests.py
+++ instagram/tests.py
@@ -21,34 +21,34 @@ class Intagram_TestCases(TestCase):
         self.assertTrue(isinstance(self.profile,Profile))
         self.assertTrue(isinstance(self.new_image,Image))
 
-    # def test_save_method(self):
-    #     self.new_image.save_image()
-    #     all_objects = Image.objects.all()
-    #     self.assertTrue(len(all_objects)>0)
+    def test_save_method(self):
+        self.new_image.save_image()
+        all_objects = Image.objects.all()
+        self.assertTrue(len(all_objects)>0)
 
-#     def test_delete_method(self):
-#         self.new_image.save_image()
-#         filtered_object = Image.objects.filter(author='cate')
-#         Image.delete_image(filtered_object)
-#         all_objects = Image.objects.all()
-#         self.assertTrue(len(all_objects) == 0)
+    def test_delete_method(self):
+        self.new_image.save_image()
+        filtered_object = Image.objects.filter(author='cate')
+        Image.delete_image(filtered_object)
+        all_objects = Image.objects.all()
+        self.assertTrue(len(all_objects) == 0)
 
-#     def test_display_all_objects_method(self):
-#         self.new_image.save_image()
-#         all_objects = Image.retrieve_all()
-#         self.assertEqual(all_objects.author,'cate')
+    def test_display_all_objects_method(self):
+        self.new_image.save_image()
+        all_objects = Image.retrieve_all()
+        self.assertEqual(all_objects.author,'cate')
 
 
-#     def test_update_single_object_property(self):
-#         self.new_image.save_image()
-#         filtered_object =Image.update_image('cate')
-#         fetched = Image.objects.get(author='cate')
-#         self.assertEqual(fetched.author,'cate')
-#     def test_get_image_by_id(self):
-#         self.new_image.save_image()
-#         fetched_image = Image.get_image_by_id(1)
-#         self.assertEqual(fetched_image.id,1)
-#     def test_search_by_username(self):
-#         self.profile.save_profile()        
-#         fetch_specific = Profile.objects.get(user=1)
-#         self.assertTrue(fetch_specific.id==1)
+    def test_update_single_object_property(self):
+        self.new_image.save_image()
+        filtered_object =Image.update_image('cate')
+        fetched = Image.objects.get(author='cate')
+        self.assertEqual(fetched.author,'cate')
+    def test_get_image_by_id(self):
+        self.new_image.save_image()
+        fetched_image = Image.get_image_by_id(1)
+        self.assertEqual(fetched_image.id,1)
+    def test_search_by_username(self):
+        self.profile.save_profile()        
+        fetch_specific = Profile.objects.get(user=1)
+        self.assertTrue(fetch_specific.id==1)
